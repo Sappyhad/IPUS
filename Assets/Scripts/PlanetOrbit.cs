8e792bd016a93d0b4df43e8a1e962a805891a0ca
Sappyhad <jakub.bylinski@gmail.com>
2024-01-08 23:29:52 +0100

    bug fixes, added player movement

--- Assets/Scripts/PlanetOrbit.cs
+++ Assets/Scripts/PlanetOrbit.cs
@@ -1,62 +1,47 @@
-// using UnityEngine;
-
-// public class PlanetOrbit : MonoBehaviour
-// {
-//     public Transform sun;
-//     public float RotationSpeed = 10f;
-
-//     private Rigidbody rb;
-
-//     void Start()
-//     {
-//         rb = GetComponent<Rigidbody>();
-//         rb.useGravity = false; // Wyłącz grawitację, ponieważ używamy siły do poruszania obiektu
-//     }
-
-//     void FixedUpdate()
-//     {
-//         // Oblicz wektor od obiektu do słońca
-//         Vector3 toSun = sun.position - transform.position;
-
-//         // Wyświetl informacje o odległości i wektorze siły
-//         Debug.Log("Odległość do słońca: " + Vector3.Distance(transform.position, sun.position));
-//         Debug.DrawRay(transform.position, toSun.normalized * 5f, Color.red); // Wizualizacja wektora siły
-
-//         // Oblicz siłę grawitacyjną zgodnie z prawem powszechnego ciążenia Newtona
-//         // F = G * ((m1*m2)/r^2)
-//         float gravitationalForce = 6.674f * Mathf.Pow(10, -11) * ((rb.mass * sun.GetComponent<Rigidbody>().mass) / toSun.sqrMagnitude);
-//         Debug.Log("Siła grawitacyjna obliczenia = " + gravitationalForce);
-
-//         // Dodaj siłę skierowaną do słońca
-//         rb.AddForce(toSun.normalized * gravitationalForce);
-
-//         // Obracaj obiekt wokół osi pionowej (opcjonalne, aby obiekt był zawsze zwrócony w kierunku słońca)
-//         transform.Rotate(Vector3.up, RotationSpeed * Time.fixedDeltaTime);
-//     }
-// }
-
 using UnityEngine;
 
 public class PlanetOrbit : MonoBehaviour
 {
+    public float rotationSpeed = 10f;
+    public float orbitSpeed = 1f;
     public Transform sun;
-    public float RotationSpeed = 10f;
 
-    private Rigidbody rb;
+    private float angle = 0f;
 
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.useGravity = false; // Wyłącz grawitację, ponieważ używamy siły do poruszania obiektu
+        
+        if (sun == null)
+        {
+            // Przypisz zmienną sun ręcznie, jeśli nie została przypisana w inspektorze
+            sun = GameObject.Find("Sun").transform; // Zastąp "Sun" nazwą obiektu słońca w hierarchii
+            if (sun == null)
+            {
+                Debug.LogError("Nie znaleziono obiektu słońca.");
+                return;
+            }
+        }
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        Vector3 toSun = sun.position - transform.position;
-        Debug.Log("toSun = " + toSun);
-        float gravitationalForce = 6.674f * Mathf.Pow(10, -11) * ((rb.mass * sun.GetComponent<Rigidbody>().mass) / toSun.sqrMagnitude);
-        rb.AddForce(toSun.normalized * gravitationalForce);
+        RotateAroundOwnAxis();
+        OrbitAroundSun();
+    }
 
-        transform.Rotate(Vector3.up, RotationSpeed * Time.fixedDeltaTime);
+   private void RotateAroundOwnAxis()
+    {
+        Debug.Log("Rotating around own axis");
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OrbitAroundSun()
+    {
+        float xPos = Mathf.Sin(angle);
+        float zPos = Mathf.Cos(angle);
+
+        transform.localPosition = new Vector3(xPos, 0f, zPos);
+
+        angle += orbitSpeed * Time.deltaTime;
     }
 }
