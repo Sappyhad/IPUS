a929d4b82ad4a66c61310972685e5768fa88b310
Sappyhad <jakub.bylinski@gmail.com>
2024-01-11 10:32:12 +0100

    added working main menu, new scene for settings (scene in dev), and corrected pluto orbit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // public void Play(){
+    //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    // }
+    public void Play()
+    {
+        // Ładuje scenę z grą (zmień "GameScene" na nazwę twojej sceny z grą)
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Settings()
+    {
+        // Ładuje scenę z opcjami (zmień "SettingsScene" na nazwę twojej sceny z opcjami)
+        SceneManager.LoadScene("SettingsScene");
+    }
+
+    public void QuitGame()
+    {
+        // Wychodzi z gry (tylko w buildzie; w edytorze może nie działać)
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+    
+    // public void QuitTheGame(){
+    //     Application.Quit();
+    //     Debug.Log("Player Has Quit The Game");
+    // }
+}
